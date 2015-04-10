fa0a6b4de225525befc3131a4456bd15d702f587
Galch <herz1012@gmail.com>
2015-04-11 01:05:46 +0900

    add state machine : idle -> vibe -> random swing

--- cathack/src/main.c
+++ cathack/src/main.c
@@ -11,14 +11,21 @@
 	PB1 ADC1 CH9
 	PB11 TIM2 CH4
 */
-
+const uint8_t STATE_IDLE = 0;
+const uint8_t STATE_VIBE = 1;
+const uint8_t STATE_SWING = 2;
 int main(void) {
+	uint8_t state = STATE_IDLE;
+
 	uint16_t proc_cnt = 1;	 
 	uint16_t BASE_Threshold = 0;
 	uint16_t adc_val = 0;	
 
 	uint16_t touch_cnt = 0;	
-   	uint16_t servo_offset = 0;
+	uint16_t idle_cnt = 0;	
+   	uint16_t servo_offset = 50;
+
+	uint8_t i,j;
 	// init peripheral
 	PWM_Configuration();
 	GPIO_Configuration(); 	
@@ -31,10 +38,8 @@ int main(void) {
 	// ADC port calibration		
 	while(proc_cnt++ < 1000)
 	{											   	    
-	    adc_val = GetTouch();
-		// recursive average
-		//BASE_Threshold = (proc_cnt-1) * BASE_Threshold / proc_cnt + adc_val / proc_cnt;	
-		// find Max
+	    adc_val = GetTouch();	
+		// find Maximum in idle state
 		if(BASE_Threshold < adc_val)
 			BASE_Threshold = adc_val;
 								 
@@ -47,27 +52,58 @@ int main(void) {
 	LED_G_OFF;
 
     while (1)
-    {   	    
-	    adc_val = GetTouch();
+    {
+		adc_val = GetTouch();
 
 		if(adc_val < BASE_Threshold)
+		{
 			touch_cnt++;
-
-		if(touch_cnt > 5)
-		{	  		  
-			touch_cnt = 0;
-			servo_offset = (TIM2->CNT % 90);  
-			Delay_ms(300);
+			idle_cnt = 0;
 		}
-		if(touch_cnt) 
-		{				
-			LED_B_ON;				 
-			PWM_SetP(servo_offset + (TIM2->CNT % 10));	
-		}			
 		else
-			LED_B_OFF; 
-						
-		Delay_ms(10);
+			idle_cnt++;
+					
+		switch(state)
+		{
+			case STATE_IDLE:
+				if(touch_cnt > 5)
+					state = STATE_VIBE;
+				else if(idle_cnt > 30000)
+					state = STATE_SWING;
+						    					
+				break;
+			case STATE_VIBE:	
+				PWM_SetP(servo_offset + (TIM2->CNT % 10));
+				if(touch_cnt > 25) {
+					servo_offset = (TIM2->CNT % 90);
+					touch_cnt = 0;
+				} else if (idle_cnt > 5000)
+				{
+					state = STATE_SWING;	
+				}			
+				break;
+			case STATE_SWING:
+				for(j=0; j<1+(TIM2->CNT % 5); j++)
+				{
+					for(i=0; i<100; i++)
+					{
+						PWM_SetP(i);
+						Delay_ms(1+(TIM2->CNT % 40));
+					}
+					for(i=100; i>0; i--)
+					{
+						PWM_SetP(i);   
+						Delay_ms(1+(TIM2->CNT % 40));
+					}
+				}
+				PWM_SetP((TIM2->CNT % 100));	  			   
+				touch_cnt = 0;
+				idle_cnt = 0;
+				state = STATE_IDLE;
+					
+				break;
+		}			 
+		Delay_ms(1);
     }
 
 	return 0;
